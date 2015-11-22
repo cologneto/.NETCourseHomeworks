66aa6b7c0bb6a12d2c2ba4a4cfffc204359e7e40
cologneto <cologneto@gmail.com>
2015-11-22 11:05:28 +0200

    Pine version 2.0 commit

---
+++ DataTypes/11.PineVersion2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.PineVersion2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter an odd number");
+            int n = int.Parse(Console.ReadLine());
+            int oCounter = 1;
+            int emptySpaceCounter = n / 2;
+
+            do
+            {
+                for (int j = 0; j < emptySpaceCounter; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int i = 0; i < oCounter; i++)
+                {
+                    Console.Write("o");
+                }
+
+                oCounter += 2;
+                emptySpaceCounter--;
+
+                Console.WriteLine();
+            } while (oCounter != n);
+
+            for (int k = 0; k < n/2; k++)
+            {
+                Console.Write(" ");
+                
+            }
+            Console.Write("|");
+            Console.WriteLine();
+            Console.WriteLine("Press any key");
+            Console.ReadKey();
+        }
+    }
+}
